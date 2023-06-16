a2f8a908f24c3d319fa58730e6b8e02a95e00ed1
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-06-16 10:47:57 -0300

    update nav

--- src/components/nav/nav.vue
+++ src/components/nav/nav.vue
@@ -2,11 +2,28 @@
 </script>
 
 <template>
-    <nav>
-        nav
+    <nav class="nav">
+        <p>Inicio</p>
+        <p>Trabajos</p>
     </nav>
 </template>
 
 <style scoped>
-
+    .nav {
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        gap: 5px;
+    }
+    .nav p {
+        font-size: 20px;
+        cursor: pointer;
+        color:aliceblue;
+        margin-left: 10px;
+    }
+    .nav p:hover {
+        color: antiquewhite;
+        transform: all .3s linear;
+        transform: scale(1.1);
+    }
 </style>
